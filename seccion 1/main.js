501006a4ae02f6a25807422366a48eb8763d6887
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-11-20 20:41:06 -0500

    solucion 5

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -1,18 +1,19 @@
 addEventListener("DOMContentLoaded", (e) =>{
     form=document.querySelector("#form")
-    form.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        let data=Object.fromEntries(new FormData(e.target))
-        let dolares=Number(data.dolares)
-        let pesos=4800
-        let conver=dolares*pesos
+        let form=document.querySelector("#form")
     
-        let mytable=document.querySelector("tbody");
-        mytable.insertAdjacentHTML("beforeend",`
-        <tr>
-            <td>${dolares}</td>
-            <td>${conver}</td>
-        </tr>
-        `)
+        form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        let data = Object.fromEntries(new FormData(e.target))
+        let observaciones= data.observaciones
+        let hora= (data.hora)
+        let tabla=document.querySelector("tbody");
+        tabla.insertAdjacentHTML("beforeend",`
+            <tr>
+                <td> ${observaciones}  </td>
+                <td> ${hora} </td>
+            </tr>
+            `)
+
     })
-    })
+})
